97a1f413d7ce5853d4249608fa6590411612098f
Van Gestel Jasper <vgesteljasper@gmail.com>
2018-04-27 19:16:39 +0200

    all header names are now lowercase

--- src/Request/Headers.php
+++ src/Request/Headers.php
@@ -4,4 +4,26 @@ use \VanGestelJasper\Collection\Collection;
 
 class Headers extends Collection
 {
+
+  /**
+   * Request constructor.
+   */
+  public function __construct($headers)
+  {
+    // function to loop map over keys and values
+    function array_map_assoc(callable $f, array $a) {
+      return array_column(array_map($f, array_keys($a), $a), 1, 0);
+    }
+
+    // lowercase mapper function
+    $toLowerCase = function($key, $value) {
+      return [strtolower($key), $value];
+    };
+
+    // map header values to lowercase
+    $headers = array_map_assoc($toLowerCase, $headers);
+
+    // construct the collection
+    parent::__construct($headers);
+  }
 }
